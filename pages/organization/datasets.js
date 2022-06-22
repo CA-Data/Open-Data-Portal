be0f80c828f06d1b212d06fe7fe9528adb613d4f
Timothy Shambra <timothy.shambra@gmail.com>
2022-06-22 13:53:25 -0700

    fix/ component path

--- pages/organization/datasets.js
+++ pages/organization/datasets.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router'
-import BasicSelect from '../components/BasicSelect';
+import BasicSelect from '../../components/BasicSelect';
 export async function getServerSideProps(context) {
   return getFormattedData(context);
 }
